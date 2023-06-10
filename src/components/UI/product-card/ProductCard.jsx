e003af99e56c94251650f01a39c11c1e39641784
LE DINH THANH <thanhldse160337@fpt.edu.vn>
2023-06-10 22:20:08 +0700

    update productcart

--- src/components/UI/product-card/ProductCard.jsx
+++ src/components/UI/product-card/ProductCard.jsx
@@ -3,7 +3,6 @@ import '../../../style/product-card.css';
 import { useDispatch } from "react-redux";
 import { cartActions } from "../../../pages/redux/cartSlice";
 import { Link } from "react-router-dom";
-import ShopDetail from "../../../pages/ShopDetail";
 
 
 const ProductCard = (props) => {
@@ -41,8 +40,20 @@ const ProductCard = (props) => {
     };
 
     return (
-
-
+        // <div className="product__item d-flex flex-column justify-content-between mt-5">
+        //     <div className="product__content">
+        //         <img className="product__img w-50" src={image} alt="Bird" />
+        //         <h5>
+        //             <Link to={`/bird/${productId}`}>{name}</Link>
+        //         </h5>
+        //     </div>
+        //     <div className="d-flex flex-column align-items-center justify-content-between">
+        //         <span className="product__price mb-2">{price} $ </span>
+        //         <button className="addTOCART__btn" onClick={addToCart}>
+        //             Add to Cart
+        //         </button>
+        //     </div>
+        // </div>
         <div className="product-tag card">
             <img src={thumbnail} alt="Product Image" className="card-img-top" />
             <div className="card-body">
@@ -54,39 +65,20 @@ const ProductCard = (props) => {
                     <br />
                     <span className="discount-price">Sale Price: <span className="discount-price-color">{price}$</span></span><br />
                     <span className="rate">Rating: {renderRating()}</span><br />
-                    <Link to={`/shop/${productId}`}>
-                        <img src={image} alt="Product Image" className="card-img-top" />
-                        <ShopDetail
-                            id={productId} />
-                    </Link>
-                    <div className="card-body">
-                        <h3 className="card-title">
-                            <Link to={`/shop/${productId}`}>{name}</Link>
-                        </h3>
-                        <p className="card-text">
-                            <span className="original-price">Price: {price}$</span>
-                            <br />
-                            <span className="discount-price">
-                                Discount Price: <span className="discount-price-color">{discountPercent}$</span>
-                            </span>
-                            <br />
-                            <span className="rate">Rating: {renderRating()}</span>
-                            <br />
->>>>>>> 8e50a3f0b306614235b75889ab5de7f30cb20f8b
-                            <span className="quantity-sold">Quantity Sold: {quantitySold}</span>
-                            {discountPercent && (
-                                <span className="discount-badge">{discountPercent}%</span>
-                            )}
+                    <span className="quantity-sold">Quantity Sold: {quantitySold}</span>
+                    {discountPercent && (
+                        <span className="discount-badge">{discountPercent}%</span>
+                    )}
 
-                            <div className="text-center mt-2">
-                                <button className="addTOCART__btn" onClick={addToCart}>
-                                    Add to Cart
-                                </button>
-                            </div>
-                        </p>
+                    <div className="text-center mt-2">
+                        <button className="addTOCART__btn" onClick={addToCart}>
+                            Add to Cart
+                        </button>
                     </div>
+                </p>
             </div>
-            );
+        </div>
+    );
 };
 
-            export default ProductCard;
+export default ProductCard;
