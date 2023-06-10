2be0a8aa07b6da65b666a6eb99d1e2dd9cf4674d
HoangChiDuong <duonghoangasd@gmail.com>
2023-06-10 09:44:17 +0700

    Update Shopping.jsx

--- src/pages/Shopping.jsx
+++ src/pages/Shopping.jsx
@@ -1,4 +1,4 @@
-aimport React from "react";
+import React from "react";
 import axios from 'axios';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
